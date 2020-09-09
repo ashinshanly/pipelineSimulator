2895663b1584e11b8932b12c606f737cb9353c0c
Ashin Shanly <ashinkoottala@gmail.com>
2020-09-09 16:26:50 +0530

    Update hazard_parser.cpp

--- hazard_parser.cpp
+++ hazard_parser.cpp
@@ -2,28 +2,25 @@
 #include <string>
 using namespace std;
 
-//This method returns whether or not a data hazard is found.
-//Make sure to check at the right pipleline stage and only when forwarding is false.
 bool dataHazard(string currentLine, string prevLine) {
 	
-	//pre-check nops
+	
 	if (prevLine == "nop" || currentLine == "nop")
 		return false;
 
-	//only need the destination of the previous line
+	
 	string dest_str = prevLine.substr(prevLine.find("$"), prevLine.find(",") - prevLine.find_first_of("$"));
 
-	//need the used argument only registers for the current line
 	string instruction = currentLine.substr(0, currentLine.find(" "));
 
-	if (instruction == "add" || instruction == "or" || instruction == "and" || instruction == "slt") {
+	if (instruction == "sub" || instruction == "add" || instruction == "or" || instruction == "and") {
 
 		string reg1 = currentLine.substr(currentLine.find(",") + 1, currentLine.find_last_of(",") - (currentLine.find(",") + 1));
 		string reg2 = currentLine.substr(currentLine.find_last_of("$"));
 
 		return dest_str == reg1 || dest_str == reg2;
 	}
-	else if (instruction == "addi" || instruction == "ori" || instruction == "slti" || instruction == "andi") {
+	else if (instruction == "addi" || instruction == "laod" || instruction == "store" || instruction == "andi") {
 		string reg1 = currentLine.substr(currentLine.find(",")+1, currentLine.find_last_of(",") - (currentLine.find(",") + 1));
 
 		return dest_str == reg1;
